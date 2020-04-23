47f26e69fe6f08a7123a3e832ee5b3c24e0ed635
GitModu <batatas@gmail.com>
2020-04-23 23:25:32 +0100

    Refactored using PWM class.

--- src/TinyL298.h
+++ src/TinyL298.h
@@ -13,17 +13,17 @@
 #ifndef _TINYL298_h
 #define _TINYL298_h
 
-#if defined (ARDUINO) || defined(ATTINY_CORE)
-#include <Arduino.h>
+#if defined(ATTINY_CORE)
 #else
-#pragma Only ATmega328 and ATTiny 85 supported.
+#pragma Only ATTiny 85 supported.
 #endif
 
 #include <stdint.h>
 #include <Fast.h>  //Fast and simple IO.
 
-#define OUTPUT_ENABLE_PIN PB4 // Chip pin 3. Fixed pin, tied to PWM on Timer1.
+#include <PWM.h>
 
+#define OUTPUT_ENABLE_PIN PIN_B4 // Chip pin 3. Fixed pin, tied to PWM on Timer1.
 
 #define UINT16_MIDDLE	((uint16_t)32767)
 #define UINT16_QUARTER	((uint16_t)16383) 
@@ -44,69 +44,44 @@ public:
 		: A1(pinA1)
 		, A2(pinA2)
 	{
-		pinMode(OUTPUT_ENABLE_PIN, OUTPUT);
-		Begin();
 	}
 
 	void Begin()
 	{
-		//Thank you Gammon. http://www.gammon.com.au/timers
-		TCCR1 = bit(CS10);					// No prescaler.
-		GTCCR = bit(COM1B1) | bit(PWM1B);   // Clear OC1B on compare.
-		OCR1B = 0;							// Duty cycle.
-		OCR1C = UINT8_MAX;					// Frequency.
+		pwm.set(1, 'B', 31250, 2);
+		pwm.start(1); //start timer.
+		SetValue(UINT16_MIDDLE);
 	}
 
 	// ||Backward|Brake|Neutral|Forward||
-	void SetValue(uint16_t value)
+	void SetValue(const uint16_t value)
 	{
 		if (value > NEUTRAL_TOP)
 		{
 			//Set forward with duty-cycle.
-			OCR1B = map(value, NEUTRAL_TOP, UINT16_MAX, 0, UINT8_MAX);
+			pwm.set_register(1, 'B', map(value, NEUTRAL_TOP, UINT16_MAX, 0, UINT16_MAX));
 			A1 = HIGH;
 			A2 = LOW;
 		}
 		else if (value <= NEUTRAL_TOP && value >= NEUTRAL_BOTTOM)
 		{
-			//Set neutral.
-			OCR1B = 0;
+			//Set neutral (Off).
+			pwm.set_register(1, 'B', 0);
 		}
-		else if(value < NEUTRAL_BOTTOM && value > BRAKE_BOTTOM)
+		else if(value < NEUTRAL_BOTTOM && value >= BRAKE_BOTTOM)
 		{
 			//Set brake with duty-cycle.
-			OCR1B = map(value, NEUTRAL_BOTTOM, BRAKE_BOTTOM, 0, UINT8_MAX);
+			pwm.set_register(1, 'B', map(value, NEUTRAL_BOTTOM, BRAKE_BOTTOM, 0, UINT16_MAX));
 			A1 = LOW;
 			A2 = LOW;
 		} 
 		else
 		{
 			//Set reverse with duty-cycle.
-			OCR1B = map(value, BRAKE_BOTTOM, 0, 0, UINT8_MAX);
+			pwm.set_register(1, 'B', map(value, BRAKE_BOTTOM, 0, 0, UINT16_MAX));
 			A1 = LOW;
 			A2 = HIGH;
 		}
 	}
-
-	inline void SetNeutral()
-	{
-
-	}
-
-	inline void SetBrake(uint16_t value)
-	{
-
-	}
-
-	inline void SetForward(uint16_t value)
-	{
-
-	}
-
-	inline void SetBackward(uint16_t value)
-	{
-		
-	}
 };
 #endif
-
