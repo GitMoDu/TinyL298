0c696910e64e9148ca1e66ef702cf3bffb971b89
GitModu <batatas@gmail.com>
2020-05-09 22:50:36 +0100

    Updated example.

--- examples/TinyMotorTester/TinyMotorTester.ino
+++ examples/TinyMotorTester/TinyMotorTester.ino
@@ -1,24 +1,28 @@
 #include <TinyL298.h>
 
 #ifdef ATTINY_CORE
-#define INPUT_PWM_PIN PB2
+#define OUTPUT_ENABLE_PIN PB1
 #define OUTPUT_A1_PIN PB0
-#define OUTPUT_A2_PIN PB1
+#define OUTPUT_A2_PIN PB4
 #else
 #pragma Only ATTiny 85 supported.
 #endif
 
+// Simulate typical RC PWM period.
 #define OUTPUT_UPDATE_PERIOD_MILLIS 20
-#define STEP_SIZE 10
 
 TinyL298 MotorDriver(OUTPUT_A1_PIN, OUTPUT_A2_PIN);
 
+const uint32_t TestDurationMillis = 10000;
+const uint32_t TestPauseMillis = 1000;
 uint32_t LastUpdated = 0;
 uint16_t TestValue = UINT16_MIDDLE;
+uint32_t TestStart = 0;
 
 void setup()
 {
 	MotorDriver.Begin();
+	TestStart = millis();
 }
 
 void loop()
@@ -26,7 +30,32 @@ void loop()
 	if (millis() - LastUpdated > OUTPUT_UPDATE_PERIOD_MILLIS)
 	{
 		LastUpdated = millis();
-		MotorDriver.SetValue(TestValue);
-		TestValue += STEP_SIZE;
+
+		uint32_t Elapsed = millis() - TestStart;
+
+		if (Elapsed > TestDurationMillis)
+		{
+			MotorDriver.SetValue(UINT16_MIDDLE);
+			TestStart = millis();
+		}
+		if (Elapsed > (TestDurationMillis - TestPauseMillis))
+		{
+			MotorDriver.SetValue(UINT16_MIDDLE);
+		}
+		else if (Elapsed <= (TestDurationMillis - TestPauseMillis)/2)
+		{
+			uint16_t value = map(Elapsed, 0, (TestDurationMillis - TestPauseMillis) /2, UINT16_MIDDLE, UINT16_MAX);
+			MotorDriver.SetValue(value);
+		}
+		else if (Elapsed > ((TestDurationMillis - TestPauseMillis) / 2) && 
+			Elapsed <= (TestDurationMillis / 2))
+		{
+			MotorDriver.SetValue(UINT16_MIDDLE);
+		}
+		else
+		{
+			uint16_t value = map(Elapsed, (TestDurationMillis - TestPauseMillis) /2, (TestDurationMillis - TestPauseMillis), UINT16_QUARTER, 0);
+			MotorDriver.SetValue(value);
+		}
 	}
 }
